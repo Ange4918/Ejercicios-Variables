5939ddadcf8d8e56e3dc1f066476c0f51e5f70d5
Ange4918 <angy4918@gmail.com>
2025-01-28 20:49:27 -0500

    Add files via upload

---
+++ EjerciciosVariables/src/ClasesVariables/Ejercicio1.java
@@ -0,0 +1,29 @@
+package ClasesVariables;
+
+public class Ejercicio1 {
+	
+	public static void main(String args[]) {
+		
+		String nombrePersona = "Angeline";
+		
+		System.out.println("Mi nombre es: " + nombrePersona);
+		
+		double x = 4.52;
+		
+		System.out.println("Décimales: " + x);
+		
+		final double dolar = 4191.84;
+		
+		System.out.println("El valor del dolar es: " + dolar);
+				
+		float PI = 3.1415927f;
+		
+		System.out.println("Primeros 7 dígitos de PI: " + PI);
+		
+		double euler = 2.718281828459045;
+		
+		System.out.println("Primeros 15 dígitos de Euler: " + euler);
+		
+	}
+
+}
---
+++ EjerciciosVariables/src/ClasesVariables/Ejercicio2.java
@@ -0,0 +1,39 @@
+package ClasesVariables;
+
+public class Ejercicio2 {
+	
+	public static void main(String args[]) {
+		
+		double a = 8.0;
+		
+		char b = 'A';
+		
+		String c = "Colombia";
+		
+		String d = "A";
+		
+		float e = 74548f;
+		
+		int numero1;
+		
+		String nombre_Persona;
+		
+		double edad;
+		
+		String Altura = "1.82";
+		
+		double alturaReal = Double.parseDouble(Altura);
+		
+		int variable = 96;
+		
+		int aa = 98;
+		
+		int x = aa;
+		
+		String y = "25";
+		
+		String g = y;
+
+	}
+
+}
